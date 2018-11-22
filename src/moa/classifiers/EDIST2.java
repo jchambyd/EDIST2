a7321a71d6cdf4da95d4ad01f496e4ee088c28a0
Jorge Cristhian Chamby Diaz <jchambyd@gmail.com>
2018-11-22 09:26:18 -0200

    Update EDIST2.java

--- src/moa/classifiers/EDIST2.java
+++ src/moa/classifiers/EDIST2.java
@@ -29,18 +29,26 @@ import moa.options.ClassOption;
 /**
  * EDIST2: Error Distance Approach for Drift Detection and Monitoring
  *
- * <p>Error Distance Approach for Drift Detection and Monitoring (EDIST2). 
- * EDIST2 was proposed in order to deal with these complex drifts
- * EDIST2 monitors the learner performance through a self-adaptive window
- * that is autonomously adjusted through a statistical hypothesis test. This
- * statistical test provides theoretical guarantees, regarding the false alarm
- * rate, which were experimentally confirmed. </p>
+ * <p>
+ * Error Distance Approach for Drift Detection and Monitoring (EDIST2). EDIST2
+ * was proposed in order to deal with these complex drifts EDIST2 monitors the
+ * learner performance through a self-adaptive window that is autonomously
+ * adjusted through a statistical hypothesis test. This statistical test
+ * provides theoretical guarantees, regarding the false alarm rate, which were
+ * experimentally confirmed.
+ * </p>
  *
- * <p>See details in:<br> Imen Khamassi, Moamar Sayed-Mouchaweh, Moez Hammami
- * Self-Adaptive Windowing Approach for Handling Complex Concept Drift. 
- * In Cognitive Computation, DOI: 10.1007/s12559-015-9341-0, Springer, 2015.</p>
+ * <p>
+ * See details in:<br>
+ * Imen Khamassi, Moamar Sayed-Mouchaweh, Moez Hammami Self-Adaptive Windowing
+ * Approach for Handling Complex Concept Drift. In Cognitive Computation, DOI:
+ * 10.1007/s12559-015-9341-0, Springer, 2015.
+ * </p>
  *
- * <p>Parameters:</p> <ul>
+ * <p>
+ * Parameters:
+ * </p>
+ * <ul>
  * <li>-l : Base Classiﬁer to train.</li>
  * <li>-n : Maximum number of error occurred in the window</li>
  * </ul>
@@ -52,19 +60,18 @@ public class EDIST2 extends AbstractClassifier implements MultiClassClassifier {
 
     private static final long serialVersionUID = 1L;
 
-    public ClassOption baseLearnerOption = new ClassOption("baseLearner", 'l',
-        "Classifier to train.", Classifier.class, "trees.HoeffdingTree");
-    
+    public ClassOption baseLearnerOption = new ClassOption("baseLearner", 'l', "Classifier to train.", Classifier.class,
+            "trees.HoeffdingTree");
+
     public IntOption numberErrorsOption = new IntOption("numberErrors", 'n',
-        "Maximum number of error occurred in the window.", 30, 1, Integer.MAX_VALUE);    
-    
+            "Maximum number of error occurred in the window.", 30, 1, Integer.MAX_VALUE);
+
     protected Classifier classifier, bkgClassifier;
     WindowEDIST2 global, current, globalTmp;
     long index;
-    
+
     @Override
-    public void resetLearningImpl()
-    {
+    public void resetLearningImpl() {
         this.classifier = ((Classifier) getPreparedClassOption(this.baseLearnerOption)).copy();
         this.bkgClassifier = this.classifier.copy();
         this.classifier.resetLearning();
@@ -76,64 +83,59 @@ public class EDIST2 extends AbstractClassifier implements MultiClassClassifier {
     }
 
     @Override
-    public void trainOnInstanceImpl(Instance inst)
-    {
+    public void trainOnInstanceImpl(Instance inst) {
         this.index++;
 
-        if (this.global == null){
-            this.global = new WindowEDIST2(inst, this.numberErrorsOption.getValue());            
+        if (this.global == null) {
+            this.global = new WindowEDIST2(inst, this.numberErrorsOption.getValue());
         }
 
-        if (!this.global.isCompleted()){
+        if (!this.global.isCompleted()) {
             this.global.buffer.add(inst);
-            if(!this.classifier.correctlyClassifies(inst)){
+            if (!this.classifier.correctlyClassifies(inst)) {
                 this.global.updateParameters(this.index);
             }
-        }
-        else{
-            if(this.current == null){
+        } else {
+            if (this.current == null) {
                 this.current = new WindowEDIST2(inst, this.numberErrorsOption.getValue());
                 this.current.lastError = this.index - 1;
             }
 
-            if(!this.current.isCompleted()){
+            if (!this.current.isCompleted()) {
                 this.current.buffer.add(inst);
-                if(!this.classifier.correctlyClassifies(inst)){
+                if (!this.classifier.correctlyClassifies(inst)) {
                     this.current.updateParameters(this.index);
                 }
-            }
-            else{
+            } else {
                 int level = this.detectedLevel(global, current);
 
-                switch(level)
-                {
-                    case 0: // In control
-                        this.global.addInstances(this.current.buffer);
-                        this.updateParatemeters(global, current);
-                        break;
-                    case 1: // Warning
-                        if(this.globalTmp == null){
-                            this.globalTmp = (WindowEDIST2) this.current.copy();
-                        }
-                        else{
-                            this.globalTmp.addInstances(this.current.buffer);
-                            this.updateParatemeters(globalTmp, current);
-                        }
-                        this.bkgClassifier.trainOnInstance(inst);
-                        break;
-                    case 2: // Drift
-                        if (this.globalTmp != null)
-                            this.global = (WindowEDIST2) this.globalTmp.copy();
-                        else
-                            this.global = (WindowEDIST2) this.current.copy();
-                        this.globalTmp = null;
-                        this.classifier = null;
-                        this.classifier = this.bkgClassifier;
-                        this.bkgClassifier = ((Classifier) getPreparedClassOption(this.baseLearnerOption)).copy();
-                        this.bkgClassifier.resetLearning();
-                        break;
-                    default:
-                        break;
+                switch (level) {
+                case 0: // In control
+                    this.global.addInstances(this.current.buffer);
+                    this.updateParatemeters(global, current);
+                    break;
+                case 1: // Warning
+                    if (this.globalTmp == null) {
+                        this.globalTmp = (WindowEDIST2) this.current.copy();
+                    } else {
+                        this.globalTmp.addInstances(this.current.buffer);
+                        this.updateParatemeters(globalTmp, current);
+                    }
+                    this.bkgClassifier.trainOnInstance(inst);
+                    break;
+                case 2: // Drift
+                    if (this.globalTmp != null)
+                        this.global = (WindowEDIST2) this.globalTmp.copy();
+                    else
+                        this.global = (WindowEDIST2) this.current.copy();
+                    this.globalTmp = null;
+                    this.classifier = null;
+                    this.classifier = this.bkgClassifier;
+                    this.bkgClassifier = ((Classifier) getPreparedClassOption(this.baseLearnerOption)).copy();
+                    this.bkgClassifier.resetLearning();
+                    break;
+                default:
+                    break;
                 }
                 // Reset current window
                 long tmp = this.current.lastError;
@@ -141,63 +143,54 @@ public class EDIST2 extends AbstractClassifier implements MultiClassClassifier {
                 this.current.lastError = tmp;
             }
         }
-        
-        this.classifier.trainOnInstance(inst);  
+
+        this.classifier.trainOnInstance(inst);
     }
 
     @Override
-    public boolean isRandomizable()
-    {
+    public boolean isRandomizable() {
         return true;
     }
 
     @Override
-    public double[] getVotesForInstance(Instance inst)
-    {
+    public double[] getVotesForInstance(Instance inst) {
         return this.classifier.getVotesForInstance(inst);
     }
 
     @Override
-    public void getModelDescription(StringBuilder out, int indent)
-    {
+    public void getModelDescription(StringBuilder out, int indent) {
     }
 
     @Override
-    protected Measurement[] getModelMeasurementsImpl()
-    {
+    protected Measurement[] getModelMeasurementsImpl() {
         Measurement[] measurements = null;
         return measurements;
     }
 
-    private int detectedLevel(WindowEDIST2 global, WindowEDIST2 current)
-    {
+    private int detectedLevel(WindowEDIST2 global, WindowEDIST2 current) {
         int level = 0;
         double mu_d = global.mu - current.mu;
-        double delta_d = Math.sqrt( Math.pow(global.delta, 2) / global.N  + Math.pow(current.delta, 2) / current.N);
+        double delta_d = Math.sqrt(Math.pow(global.delta, 2) / global.N + Math.pow(current.delta, 2) / current.N);
         double epsilon = 1.65 * delta_d;
 
         // Drift
-        if(mu_d > epsilon + delta_d)
+        if (mu_d > epsilon + delta_d)
             level = 2;
         // Warning
-        else if(mu_d > epsilon)
+        else if (mu_d > epsilon)
             level = 1;
-        
+
         return level;
     }
 
-    private void updateParatemeters(WindowEDIST2 tglobal, WindowEDIST2 tcurrent)
-    {
+    private void updateParatemeters(WindowEDIST2 tglobal, WindowEDIST2 tcurrent) {
         tglobal.mu = (1 / (tglobal.N + tcurrent.N)) * (tglobal.N * tglobal.mu + tcurrent.N + tcurrent.mu);
 
-        tglobal.delta = Math.sqrt((((tglobal.N * Math.pow(tglobal.delta, 2)) + 
-                                  (tcurrent.N * Math.pow(tcurrent.delta, 2))) / 
-                                  (tglobal.N + tcurrent.N)
-                                 ) + 
-                                 (((tglobal.N * tcurrent.N) / Math.pow(tglobal.N + tcurrent.N, 2)) * 
-                                  (Math.pow(tglobal.mu - tcurrent.mu, 2))
-                                 )
-                                );
+        tglobal.delta = Math
+                .sqrt((((tglobal.N * Math.pow(tglobal.delta, 2)) + (tcurrent.N * Math.pow(tcurrent.delta, 2)))
+                        / (tglobal.N + tcurrent.N))
+                        + (((tglobal.N * tcurrent.N) / Math.pow(tglobal.N + tcurrent.N, 2))
+                                * (Math.pow(tglobal.mu - tcurrent.mu, 2))));
 
         tglobal.N += tcurrent.N;
 
@@ -205,12 +198,11 @@ public class EDIST2 extends AbstractClassifier implements MultiClassClassifier {
     }
 
     /**
-     * Inner class that represents a single data window characterized by:
-     * error number (N), error distance mean (mu) and error distance standard 
-     * deviation (delta).
+     * Inner class that represents a single data window characterized by: error
+     * number (N), error distance mean (mu) and error distance standard deviation
+     * (delta).
      */
-    protected final class WindowEDIST2 extends AbstractMOAObject 
-    {
+    protected final class WindowEDIST2 extends AbstractMOAObject {
         private static final long serialVersionUID = 1L;
         // Buffer os instances
         protected Instances buffer;
@@ -226,9 +218,8 @@ public class EDIST2 extends AbstractClassifier implements MultiClassClassifier {
         public boolean completed;
         // Index last error
         public long lastError;
-        
-        public WindowEDIST2(Instance inst, int maxN)
-        {
+
+        public WindowEDIST2(Instance inst, int maxN) {
             this.mu = 0;
             this.delta = 0;
             this.N = 0;
@@ -236,43 +227,37 @@ public class EDIST2 extends AbstractClassifier implements MultiClassClassifier {
             this.buffer = new Instances(inst.dataset());
             this.maxN = maxN;
         }
-        
-        public WindowEDIST2(Instances instances)
-        {
+
+        public WindowEDIST2(Instances instances) {
             this.mu = 0;
             this.delta = 0;
             this.N = 0;
             this.lastError = 0;
             this.buffer = instances;
         }
-        
-        public boolean isCompleted()
-        {
+
+        public boolean isCompleted() {
             return this.completed;
         }
 
-        public void addInstances(Instances instances)
-        {
+        public void addInstances(Instances instances) {
             for (int i = 0; i < instances.numInstances(); i++) {
                 this.buffer.add(instances.get(i));
             }
         }
 
-        public void updateParameters(long indexError)
-        {
+        public void updateParameters(long indexError) {
             long d = indexError - this.lastError;
 
-            this.mu = (this.N / (double) (this.N + 1)) * this.mu + d / (double)(this.N + 1);
-            
-            this.delta = Math.sqrt(
-                                    (N - 1) * Math.pow(this.delta, 2) / (this.N + Double.MIN_VALUE) +
-                                    Math.pow(d - this.mu , 2) / (double) (this.N + 1)
-                                  ); 
+            this.mu = (this.N / (double) (this.N + 1)) * this.mu + d / (double) (this.N + 1);
+
+            this.delta = Math.sqrt((N - 1) * Math.pow(this.delta, 2) / (this.N + Double.MIN_VALUE)
+                    + Math.pow(d - this.mu, 2) / (double) (this.N + 1));
             this.N += 1;
 
             this.lastError = indexError;
 
-            if(this.N == this.maxN)
+            if (this.N == this.maxN)
                 this.completed = true;
         }
 
